3b74faed424f99e31bf41b2c6e82657d8e82342b
Abhishek Yadav <105651140+iamabhiCH@users.noreply.github.com>
2022-12-23 13:25:15 +0530

    Add files via upload
    First Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const hamburger = document.querySelector('.hamburger');
+const hamburger_icon = hamburger.querySelector('span');
+const mobile_menu = document.querySelector('.mobile-menu');
+
+hamburger.addEventListener('click', () => {
+	hamburger_icon.innerText = hamburger_icon.innerText === 'menu' 
+		? 'close'
+		: 'menu';
+	
+	mobile_menu.classList.toggle('is-open');
+})
